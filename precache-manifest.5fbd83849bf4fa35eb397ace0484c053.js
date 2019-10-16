aba7443ab93ef25bc242e510c2a0d5cf90d8a376
Cato Perez <perezcatoc@gmail.com>
2019-10-16 04:00:09 +0000

    Updates

---
+++ precache-manifest.5fbd83849bf4fa35eb397ace0484c053.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d339d895a8eb0d956e0544fd48d842",
+    "url": "/Golden-cloud/index.html"
+  },
+  {
+    "revision": "4cedfb2b9f8134d6ea95",
+    "url": "/Golden-cloud/static/css/main.e7c5c1ec.chunk.css"
+  },
+  {
+    "revision": "070796706c58a4ed2cef",
+    "url": "/Golden-cloud/static/js/2.7d5155f4.chunk.js"
+  },
+  {
+    "revision": "4cedfb2b9f8134d6ea95",
+    "url": "/Golden-cloud/static/js/main.f545f522.chunk.js"
+  },
+  {
+    "revision": "912265964324892c9fc8",
+    "url": "/Golden-cloud/static/js/runtime-main.230ef535.js"
+  },
+  {
+    "revision": "116fa6476932f02685ff9085c290602e",
+    "url": "/Golden-cloud/static/media/accra.116fa647.jpg"
+  },
+  {
+    "revision": "5210bb53f4a2756d91f26bc9384a9a91",
+    "url": "/Golden-cloud/static/media/accra_mariott_hotel.5210bb53.jpg"
+  },
+  {
+    "revision": "bdbf3ea19a0543b0b92244bbbea4e2fa",
+    "url": "/Golden-cloud/static/media/adventure.bdbf3ea1.jpg"
+  },
+  {
+    "revision": "1765db28aae69754618814afbd2296db",
+    "url": "/Golden-cloud/static/media/bg2.1765db28.png"
+  },
+  {
+    "revision": "822078e2ecf89b2dc0c7b83af3331181",
+    "url": "/Golden-cloud/static/media/capecoast.822078e2.JPG"
+  },
+  {
+    "revision": "ff171984f450577bfb01f59f054e3be5",
+    "url": "/Golden-cloud/static/media/con.ff171984.jpg"
+  },
+  {
+    "revision": "0b3524c643e9ed1ef8acff71272d9c45",
+    "url": "/Golden-cloud/static/media/dubai.0b3524c6.jpg"
+  },
+  {
+    "revision": "c18184038743d6f44caea107b907dee4",
+    "url": "/Golden-cloud/static/media/eiffeltower.c1818403.jpg"
+  },
+  {
+    "revision": "869c4ae02e7e883edd6859f2984da050",
+    "url": "/Golden-cloud/static/media/kakum.869c4ae0.jpg"
+  },
+  {
+    "revision": "1c32151a3a0d5c7faebf7baa06800deb",
+    "url": "/Golden-cloud/static/media/kempinski.1c32151a.jpg"
+  },
+  {
+    "revision": "fe191b668977c440c78fa526956f9997",
+    "url": "/Golden-cloud/static/media/labadibeachhotel.fe191b66.jpg"
+  },
+  {
+    "revision": "b090eaab81d19460d4f06e9c7569607b",
+    "url": "/Golden-cloud/static/media/london.b090eaab.jpg"
+  },
+  {
+    "revision": "fd504322f1bde105363dca35ab14ba40",
+    "url": "/Golden-cloud/static/media/mole.fd504322.jpg"
+  },
+  {
+    "revision": "099b2939a9192d21e20dc497db6ca386",
+    "url": "/Golden-cloud/static/media/singapore.099b2939.jpg"
+  },
+  {
+    "revision": "971aa90b9d74179baee4e33f52c3368b",
+    "url": "/Golden-cloud/static/media/villa_monticello.971aa90b.jpg"
+  },
+  {
+    "revision": "88fdfc3c50c2d54434a5a21303acc7ec",
+    "url": "/Golden-cloud/static/media/webapp.88fdfc3c.jpg"
+  }
+]);
